47f8736bbedc8e6fd52564ef26db9b632c901984
Eliacobe <114414469+Eliacobe@users.noreply.github.com>
2025-12-12 16:11:59 +0000

    Fix gateway proxy POST by removing body parser

--- services/api-gateway/index.js
+++ services/api-gateway/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-app.use(bodyParser.json());
 
 // ---- PROXY ROUTES ----
 
